c4985bb0714e6f6c49dd48c65021f44c87a99d1c
RyanWalber <144794932+RyanWalber@users.noreply.github.com>
2025-10-01 15:06:07 -0300

    1.1

--- My project/Assets/Scripts/PlayerMovement.cs
+++ My project/Assets/Scripts/PlayerMovement.cs
@@ -1,21 +1,19 @@
 using UnityEngine;
-
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
     private Rigidbody rb;
-
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
-
     void FixedUpdate()
     {
-        float moveX = Input.GetAxis("Horizontal"); // A/D ou setas
-        float moveZ = Input.GetAxis("Vertical");   // W/S ou setas
-
-        Vector3 movement = new Vector3(moveX, 0, moveZ);
-        rb.AddForce(movement * speed);
+        float moveHorizontal = Input.GetAxis("Horizontal"); // A/D ou setas
+        float moveVertical = Input.GetAxis("Vertical");     // W/S ou setas
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        Vector3 newVelocity = movement * speed;
+        newVelocity.y = rb.velocity.y; // mantém a gravidade
+        rb.velocity = newVelocity;
     }
 }
